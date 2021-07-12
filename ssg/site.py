ea949c53cce77eb00c0fc1045f9c847d840e6393
Todd <todd@tuomala.org>
2021-07-11 18:11:59 -0700

    Completed module

---
+++ ssg/site.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+
+class Site:
+    def __init__(self, source, dest):
+        self.source = Path(source)
+        self.dest = Path(dest)
+
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        
+
