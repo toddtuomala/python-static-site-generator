339cb90e80a51502f7fa24334446e35522157b88
Todd <todd@tuomala.org>
2021-07-25 16:32:34 -0700

    Completed Module

--- ssg/site.py
+++ ssg/site.py
@@ -8,7 +8,7 @@ class Site:
 
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
-        directory.makedir(parents=True, exist_ok=True)
+        directory.mkdir(parents=True, exist_ok=True)
 
 
     def build(self):
